059afc813783c50e6b662bd9c0a034e48a39a963
mooojn <moojntariq@gmail.com>
2026-05-31 14:38:17 +0500

    script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Manrope } from "next/font/google";
+import Script from "next/script";
 import Navbar from "./components/Navbar";
 import { FloatingCall } from "./components/FloatingCTA";
 import { ThemeProvider } from "./components/ThemeProvider";
@@ -33,6 +34,18 @@ export default function RootLayout({
       <body
         className={`${manrope.variable} ${plexMono.variable} antialiased`}
       >
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9JS0TPS9YF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9JS0TPS9YF');
+          `}
+        </Script>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
