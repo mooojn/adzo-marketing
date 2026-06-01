e2be5e227f19a8c673956daebe9703e7593e62b3
mooojn <moojntariq@gmail.com>
2026-06-01 17:39:12 +0500

    px

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout({
       <body
         className={`${manrope.variable} ${plexMono.variable} antialiased`}
       >
+        {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-9JS0TPS9YF"
           strategy="afterInteractive"
@@ -46,6 +47,32 @@ export default function RootLayout({
             gtag('config', 'G-9JS0TPS9YF');
           `}
         </Script>
+
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function()
+            {n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '952483951120041');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=952483951120041&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
